0100aa1215e194d5977f63db55f4551a3b24419b
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-12 04:40:29 -0500

    Fix linter issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -12,5 +14,4 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     get '/messages', to: 'messages#index'
   end
-  
 end
